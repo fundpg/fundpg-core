b0c9b4f0932bb8191a63c3896e237257c29da0a5
Dylan Steck <dylancsteck@gmail.com>
2023-03-04 16:36:31 -0500

    Abi not running

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,26 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useAccount } from 'wagmi'
+import { useAccount, usePrepareContractWrite, useContractWrite } from 'wagmi'
 
 import { Account } from '../components'
 
 function Page() {
   const { isConnected } = useAccount()
+
+  const abi = {"ok":true,"abi":[{"inputs":[{"internalType":"address","name":"_depositToken","type":"address"},{"internalType":"address","name":"_strategyAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"donation","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"depositToken","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"depositAmount","type":"uint256"},{"internalType":"uint256","name":"allocationPercentage","type":"uint256"}],"name":"depositUnderlyingOnBehalf","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"userAddress","type":"address"}],"name":"getUserBalance","outputs":[{"internalType":"uint256","name":"totalValue","type":"uint256"},{"internalType":"uint256","name":"userWithdrawAmount","type":"uint256"},{"internalType":"uint256","name":"donatedYield","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"strategyAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"dstAddress","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferYield","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"userAllocation","type":"uint256"},{"internalType":"uint256","name":"userPrincipal","type":"uint256"},{"internalType":"uint128","name":"initialLiquidityIndex","type":"uint128"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdrawAllUnderlyingOnBehalf","outputs":[],"stateMutability":"nonpayable","type":"function"}]}
+  const { config } = usePrepareContractWrite({
+    address: '0x3a09D405F23373c590e1DD247B616d26B6B8d5C4',
+    abi: abi,
+    functionName: 'fundPgVault',
+  })
+  const { data, isLoading, isSuccess, write } = useContractWrite(config)
+
   return (
     <>
       <h1>wagmi + RainbowKit + Next.js</h1>
 
       <ConnectButton />
       {isConnected && <Account />}
+      <button disabled={!write} onClick={() => write?.()}>Write</button>
     </>
   )
 }
