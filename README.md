60a6c152529796ce115866c2895484f05ca1b3c6
Dylan Steck <dylancsteck@gmail.com>
2023-03-09 17:00:33 -0500

    pushing w approve working

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ We are currently working on a proof-of-concept for the ETHDenver Buildathon, whe
 Our team consists of experienced developers and experts in the DeFi space who are passionate about using technology for social good. We believe that FundPG has the potential to revolutionize the way we think about donations and make a positive impact on society.
 
 # Demo
-Check out the demo at https://funding-public-goods-7fp6.vercel.app/
+Check out the demo at https://fundpg.xyz
 
 You may issue yourself Aave's test DAI tokens at https://staging.aave.com/ 
--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -6,7 +6,7 @@ export function Account() {
   const { address } = useAccount()
   const { data: ensName } = useEnsName({ address })
 
-  const [approvalVal, setApprovalVal] = useState('0')
+  const [approvalVal, setApprovalVal] = useState('0 ETH')
   const [depositVal, setDepositVal] = useState('0')
   const [allocationVal, setAllocationVal] = useState('0')
 
@@ -71,16 +71,16 @@ export function Account() {
       case 'Approve':
         return(
         <center>
-          <div className="mt-5 p-2 w-64 flex flex-col justify-center items-center">
+          <div className="mt-5 p-2 w-full flex flex-col justify-center items-center">
             <input
-              className="rounded-md px-4 py-2 w-full text-center focus:outline-none text-lg font-medium" 
+              className="rounded-md px-6 py-4 w-full text-center focus:outline-none text-lg font-medium" 
               type="number" 
-              placeholder="0"
+              placeholder="0 ETH"
               value={approvalVal} 
-              onChange={(e) => setApprovalVal(e.target.value)} 
+              onChange={(e) => setApprovalVal(e.target.value)}
             />
             <button 
-              className="mt-5 px-4 py-2 rounded-md bg-white font-medium"
+              className="mt-4 px-2 py-1 text-sm rounded-md bg-white font-medium"
               disabled={!writeDaiApprove} 
               onClick={() => writeDaiApprove?.()}
             >
@@ -95,13 +95,15 @@ export function Account() {
       case 'Deposit': 
         return(
           <div className="mt-3">
-            <label className="block mb-2 text-sm font-medium text-gray-900">{`Amount: ${depositVal}`}</label>
-            <input 
-              className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-              type="range" 
-              value={depositVal} 
-              onChange={(e) => setDepositVal(e.target.value)} />
-
+            <div className="mt-1 flex mb-2">
+              <label className="inline-block mr-2 text-sm font-medium text-gray-900">Amount:</label>
+              <input 
+                className="inline-block w-full h-4 bg-transparent text-black outline-none mt-1"
+                type="text" 
+                placeholder="0"
+                value={depositVal} 
+                onChange={(e) => setDepositVal(e.target.value)} />
+            </div>
             <label className="block mb-2 text-sm font-medium text-gray-900">{`Allocation: ${allocationVal}%`}</label>
             <input 
               className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
@@ -110,7 +112,7 @@ export function Account() {
               onChange={(e) => setAllocationVal(e.target.value)} />
 
             <button 
-              className="mt-2 rounded-md bg-white text-black px-4 py-2"
+              className="mt-4 px-2 py-1 text-sm rounded-md bg-white font-medium mb-2"
               disabled={!writeDeposit} 
               onClick={() => writeDeposit?.()}>
                 Deposit
@@ -125,7 +127,7 @@ export function Account() {
           <div className="mt-5">
             <center>
               <button 
-                className="mt-2 rounded-md bg-white text-black px-4 py-2"
+                className="mt-10 rounded-md bg-white text-black px-4 py-2"
                 disabled={!writeWithdraw} 
                 onClick={() => writeWithdraw?.()}>
                   Withdraw All
@@ -139,17 +141,20 @@ export function Account() {
     }
   }
 
+  // const exampleTxnArray = [
+  //   { amount: 'Deposited 0.64 ETH', date: '12 days ago' },
+  //   { amount: 'Deposited 0.32 ETH', date: '26 days ago' },
+  //   { amount: 'Deposited 0.53 ETH', date: '8 days ago' },
+  //   { amount: 'Deposited 0.27 ETH', date: '17 days ago' },
+  //   { amount: 'Deposited 0.76 ETH', date: '22 days ago' },
+  //   { amount: 'Deposited 0.94 ETH', date: '3 days ago' },
+  //   { amount: 'Deposited 0.49 ETH', date: '19 days ago' },
+  //   { amount: 'Deposited 0.89 ETH', date: '6 days ago' },
+  //   { amount: 'Deposited 0.12 ETH', date: '11 days ago' },
+  //   { amount: 'Deposited 0.42 ETH', date: '28 days ago' },
+  // ];
   const exampleTxnArray = [
-    { amount: 'Deposited 0.64 ETH', date: '12 days ago' },
-    { amount: 'Deposited 0.32 ETH', date: '26 days ago' },
-    { amount: 'Deposited 0.53 ETH', date: '8 days ago' },
-    { amount: 'Deposited 0.27 ETH', date: '17 days ago' },
-    { amount: 'Deposited 0.76 ETH', date: '22 days ago' },
-    { amount: 'Deposited 0.94 ETH', date: '3 days ago' },
-    { amount: 'Deposited 0.49 ETH', date: '19 days ago' },
-    { amount: 'Deposited 0.89 ETH', date: '6 days ago' },
-    { amount: 'Deposited 0.12 ETH', date: '11 days ago' },
-    { amount: 'Deposited 0.42 ETH', date: '28 days ago' },
+    { amount: 'Deposited 0.64 ETH', date: '12 days ago' }
   ];
 
   return (
@@ -162,17 +167,17 @@ export function Account() {
           <div className="w-2/3 bg-[#D9D9D9] rounded-lg p-3 h-48">
             <div className="flex justify-between rounded-lg bg-[#989898] pt-.5 pb-.5">
               <button 
-                className={`px-2 py-1 rounded-md l-0 ${focused === 'Approve' && 'bg-[#7A7A7A] text-white'} text-gray-700 font-medium`}
+                className={`px-2 py-1 rounded-md l-0 ${focused === 'Approve' && 'bg-[#7A7A7A]'} text-black font-medium text-sm`}
                 onClick={() => focused !== 'Approve' && setFocused('Approve')}>
                 Approve
               </button>
               <button 
-                className={`px-2 py-1 rounded-md ${focused === 'Deposit' && 'bg-[#7A7A7A] text-white'} text-gray-700 font-medium`}
+                className={`px-2 py-1 rounded-md ${focused === 'Deposit' && 'bg-[#7A7A7A]'} text-black font-medium text-sm`}
                 onClick={() => focused !== 'Deposit' && setFocused('Deposit')}>
                 Deposit
               </button>
               <button 
-                className={`px-2 py-1 rounded-md r-0 ${focused === 'Withdraw' && 'bg-[#7A7A7A] text-white'} text-gray-700 font-medium`}
+                className={`px-2 py-1 rounded-md r-0 ${focused === 'Withdraw' && 'bg-[#7A7A7A]'} text-black font-medium text-sm`}
                 onClick={() => focused !== 'Withdraw' && setFocused('Withdraw')}>
                 Withdraw
               </button>
