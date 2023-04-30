43bb70182ae981f1d73333633cb5a7ac9695e983
Dylan Steck <dylancsteck@gmail.com>
2023-04-30 18:34:54 -0400

    last merch page update

--- frontend/src/pages/merch.tsx
+++ frontend/src/pages/merch.tsx
@@ -11,13 +11,13 @@ export default function Merch(){
         <center>
             <div className="flex sm:flex-row flex-col p-10 ml-[5vw] lg:ml-[7.5vw]">
                 <div>
-                    <Image src={memsShirt} alt="Mems Collab Shirt" width={500} height={500} />
+                    <Image src={memsShirt} alt="Mems x Gitcoin Shirt" width={500} height={500} />
                     <Link href="https://store.gitcoin.co/products/mems-for-gitcoin-t-shirt-limited-edition">
-                        <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">IRL Only</button>
+                        <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">Mems x Gitcoin Limited Edition (irl only)</button>
                     </Link>
                 </div>
                 <div>
-                    <Image src={creaturesShirt} alt="Gitcoin x Creature World shirt" width={550} height={550} />
+                    <Image src={creaturesShirt} alt="Gitcoin x Creature World Shirt" width={550} height={550} />
                     <Link href="https://store.gitcoin.co/products/creature-shipping">
                         <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">Creatures x Gitcoin Limited Edition</button>
                     </Link>
