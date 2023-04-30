f507d6b7b7175c7034c5a67315639b0b08afd2cf
Dylan Steck <dylancsteck@gmail.com>
2023-04-30 18:10:24 -0400

    Changed text and link for Mems shirt

--- frontend/src/pages/merch.tsx
+++ frontend/src/pages/merch.tsx
@@ -12,12 +12,14 @@ export default function Merch(){
             <div className="flex sm:flex-row flex-col p-10 ml-[5vw] lg:ml-[7.5vw]">
                 <div>
                     <Image src={memsShirt} alt="Mems Collab Shirt" width={500} height={500} />
-                    <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">Sold Out</button>
+                    <Link href="https://store.gitcoin.co/products/mems-for-gitcoin-t-shirt-limited-edition">
+                        <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">IRL Only</button>
+                    </Link>
                 </div>
                 <div>
                     <Image src={creaturesShirt} alt="Gitcoin x Creature World shirt" width={550} height={550} />
                     <Link href="https://store.gitcoin.co/products/creature-shipping">
-                    <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">Creatures x Gitcoin Limited Edition</button>
+                        <button className="rounded-lg px-4 py-3 bg-[#0e76fd] text-white text-lg font-medium">Creatures x Gitcoin Limited Edition</button>
                     </Link>
                 </div>
             </div>
